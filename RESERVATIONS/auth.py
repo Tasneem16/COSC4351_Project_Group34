c9754b5b1b43fc8b29e93d4a89683ff5184015ca
Dillon Z <90734706+dzac21@users.noreply.github.com>
2022-11-30 21:31:42 -0600

    Update auth.py
    Adding comments for readability and easier understanding.

--- RESERVATIONS/auth.py
+++ RESERVATIONS/auth.py
@@ -7,9 +7,9 @@ from flask_login import login_user, logout_user, current_user
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/loginscreen', methods=['GET', 'POST'])
+@auth.route('/loginscreen', methods=['GET', 'POST'])        #Logging in
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST':        #Tells webserver to take data entered in the form
         email = request.form.get('email')
         password = request.form.get('password1')
 
@@ -18,25 +18,25 @@ def login():
             if check_password_hash(user.password, password):
                 login_user(user, remember=True)
                 flash('Logged in successfully!', category='success')
-                return redirect(url_for('views.reserve'))
+                return redirect(url_for('views.reserve'))       #if password and email matched one in database, it takes them to the screen to reserve
             else:
-                flash('Incorrect password, try again.', category='error')
+                flash('Incorrect password, try again.', category='error')       #notifies of wrong password
         else:
-            flash('Email does not exist.', category='error')
+            flash('Email does not exist.', category='error')        #notifies account with that email doesnt exist
 
     return render_template("loginscreen.html", user=current_user)
 
 
-@auth.route('/logout')
+@auth.route('/logout')      #Logging out
 def logout():
     logout_user()
-    flash('Logged out successfully!', category='success')
+    flash('Logged out successfully!', category='success')       #notifies user they have signed out
     return redirect(url_for('auth.login'))
 
 
-@auth.route('/registerpage', methods=['GET', 'POST'])
+@auth.route('/registerpage', methods=['GET', 'POST'])       #creating an account
 def sign_up():
-    if request.method == 'POST':
+    if request.method == 'POST':        #Tells webserver to take data entered in the form
         first_name = request.form.get('firstname')
         last_name = request.form.get('lastname')
         email = request.form.get('email')
@@ -49,9 +49,9 @@ def sign_up():
         user = User.query.filter_by(email=email).first()
 
         if user:
-            flash('Email already exists.', category='error')
+            flash('Email already exists.', category='error')        #checks if account exists already
         elif len(first_name) < 2 or len(first_name) > 20:
-            flash('First name must be greater than 2 and less than 20 characters.', category='error')
+            flash('First name must be greater than 2 and less than 20 characters.', category='error')       #validating all credentials entered
         elif len(last_name) < 2 or len(last_name) > 20:
             flash('Last name must be greater than 2 and less than 20 characters.', category='error')
         elif len(phone_no) != 10:
@@ -63,7 +63,7 @@ def sign_up():
         else:
             new_user = User(user_fname=first_name, user_lname=last_name, email=email, phone=phone_no, mailing_add=mail_add,
                            prefered_payment=prefer_payment, password=generate_password_hash(password1, method='sha256'))
-            db.session.add(new_user)
+            db.session.add(new_user)        #pushing to database
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
